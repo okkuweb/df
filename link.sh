1dbeb642384528d34cb71600ea2494d1c35cec59
Oskari Saari <okkuweb@gmail.com>
2018-07-13 01:17:30 +0300

    Changed the font

--- link.sh
+++ link.sh
@@ -2,5 +2,5 @@
 ln -fv ./conf/d_init.txt ./../data/init/d_init.txt
 ln -fv ./conf/init.txt ./../data/init/init.txt
 ln -fv ./conf/embark_profiles.txt ./../data/init/embark_profiles.txt
-ln -fv ./conf/Taffer_20x20.png ./../data/art/Taffer_20x20.png
+ln -fv ./conf/Cheepicus_8x8x2.png ./../data/art/Cheepicus_8x8x2.png
 ln -fv ./conf/colors.txt ./../data/init/colors.txt
