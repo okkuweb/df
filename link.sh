8f9f5660c0f14d822392934863029c5c7340c3d3
Oskari Saari <okkuweb@protonmail.com>
2020-02-09 12:38:58 +0200

    Added option for graphical dwarf fortress

--- link.sh
+++ link.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-ln -fv ./conf/d_init.txt ./../data/init/d_init.txt
-ln -fv ./conf/init.txt ./../data/init/init.txt
-ln -fv ./conf/embark_profiles.txt ./../data/init/embark_profiles.txt
-ln -fv ./conf/Cheepicus_8x8x2.png ./../data/art/Cheepicus_8x8x2.png
-ln -fv ./conf/colors.txt ./../data/init/colors.txt
+read -p "Graphical (g) or ascii (a)`echo $'\n> '`" answer
+if [ "$answer" == "g" ]; then
+    cp -fvr ./conf/graphical/* ./../data/
+else
+    cp -fvr ./conf/ascii/* ./../data/
+fi
+
